c397942f40326c8ddd5f6d8bb901b05bb749fd77
ZauberZ <Zauber@outlook.com>
2025-04-04 20:51:38 +0800

    update

--- utils/blockface_MRI.py
+++ utils/blockface_MRI.py
@@ -1,8 +1,12 @@
 import utils.Logger as loggerz
-from utils.blockface_preproc import intensity_c, b_alignMRI
+from utils.CycleGan_3D.test import b_to_T1_cyclegan
+from utils.blockface_preproc import intensity_c, b_alignMRI, correct_t1like, blockface_3Dreg, b_invetalignMRI
 
 
 def blockface_preproc():
     # intensity_c()
-    b_alignMRI()
-    b_to_T1_cyclegan()
+    # b_alignMRI()
+    # b_to_T1_cyclegan()
+    # correct_t1like()
+    blockface_3Dreg()
+    # b_invetalignMRI()
