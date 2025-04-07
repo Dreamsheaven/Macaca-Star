4f0b3603e6213460cb74d903a5e9b77e7a370dd0
ZauberZ <Zauber@outlook.com>
2025-04-07 23:03:45 +0800

    update

--- utils/fluor_blockface.py
+++ utils/fluor_blockface.py
@@ -3,6 +3,6 @@ from utils.fluor_preproc import repaire_blikefluo, fluor_SyNtoB_bySeg
 
 
 def fluor_preproc():
-    # fluor_toB_cyclegan()
-    # repaire_blikefluo()
+    fluor_toB_cyclegan()
+    repaire_blikefluo()
     fluor_SyNtoB_bySeg()
